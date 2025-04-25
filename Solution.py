eb5de3728b26a0a52fb6503bc6c2724beff790d2
CodeKid-del <anshkhanduja.ak@gmail.com>
2025-04-24 20:42:51 -0700

    Update Solution.py
    1. Added pausing/unpausing the simulation.
    
    2. Made cells toggle depending on surrounding cells

--- Solution.py
+++ Solution.py
@@ -9,11 +9,29 @@ class CGLCell(tk.Label):
         self.coord = coord
         self.master = master
         self['bg'] = 'black'
-        self.checkSurrounding()
+        self.isUpdating = True
+        # self.checkSurrounding()
         self.bind("<Button-1>", self.toggle)
+        self.after(1000,self.update)
+        # while True:
+        #     self.update()
+
 
     def giveState(self):
         return self.state
+
+    def giveCoords(self):
+        return self.coord
+
+    def isupdating(self):
+        return self.isUpdating
+
+    def stopUpdating(self):
+        self.isUpdating = False
+
+    def startUpdating(self):
+        self.isUpdating = True
+        self.update()
 
     def toggle(self, event):
         self.state = not self.state
@@ -21,62 +39,97 @@ class CGLCell(tk.Label):
             self['bg'] = 'white'
         else:
             self['bg'] = 'black'
-        print(self.giveState())
+        # print(self.giveState())
+        # print(self.checkSurrounding())
+
+    def die(self):
+        self.state = False
+        self['bg'] = 'black'
+        # print(self.giveState())
+
+    def live(self):
+        self.state=True
+        self['bg'] = 'white'
+
 
     def checkSurrounding(self):
-        master = self.master
-        # print(master.cellGrid)
-        # print(self.master.cellGrid[0].coord)
-        pass
+        '''calls the frame to check how many cells around it are alive'''
+        return self.master.checkSurroundingCoord(coord=self.giveCoords())
+
+    def update(self):
+        surroundingAliveCells = self.checkSurrounding()
+        if self.giveState():
+            if surroundingAliveCells < 2:
+                self.die()
+            if surroundingAliveCells > 3:
+                self.die()
+        else:
+            if surroundingAliveCells == 3:
+                self.live()
+        if self.isupdating():
+            self.after(1000,self.update)
 
 
 class CGLFrame(tk.Frame):
-    def __init__(self, master, height, width):
+    def __init__(self, master, rows, cols):
         tk.Frame.__init__(self, master)
         self.grid()
         # z = CGLCell(self,(0,0))
+        self.rows = rows
+        self.cols = cols
+        # print(rows,cols)
         self.cellGrid = []
-        for x in range(height):
-            for y in range(width):
-                self.cellGrid.append(CGLCell(self, (x, y)))
-                # print(self.cellGrid)
+        for r in range(rows):
+            tempList = []
+            for c in range(cols):
+                tempList.append([])
+            self.cellGrid.append(tempList)
+        # print(self.cellGrid)
+        for x in range(rows):
+            for y in range(cols):
+                # print(x,y)
+                self.cellGrid[x][y] = CGLCell(self, (x, y))
+                self.cellGrid[x][y].grid(row=x,column=y)
 
+        self.isUpdating = True
+
+        self.bind('<space>',self.toggleUpdating)
+
+        self.toggleUpdating('hi')
+
+
+    def checkSurroundingCoord(self,coord):
+        aliveCounter = 0
+        for r in range(coord[0]-1,coord[0]+2):
+            for c in range(coord[1] - 1, coord[1] + 2):
+
+                # print(r,c,self.cellGrid[r][c].giveState())
+                # print(coord)
+                # print(len(self.cellGrid))
+                if r >= self.rows:
+                    r = r%self.rows
+                if c >= self.cols:
+                    c = c%self.cols
+                if self.cellGrid[r][c].giveState():
+                    aliveCounter +=1
+        if self.cellGrid[coord[0]][coord[1]].giveState():
+            aliveCounter -= 1
+        return aliveCounter
+
+    def toggleUpdating(self,event):
+        self.isUpdating = not self.isUpdating
+        # self.cellGrid[0][0].toggle(' ')
+        # print(self.isUpdating)
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.isUpdating:
+                    self.cellGrid[r][c].startUpdating()
+                else:
+                    self.cellGrid[r][c].stopUpdating()
 
 root = tk.Tk()
 root.title("Conway's Game of Life")
 # MSG = MSGrid(root,10,10,10)
-CGL = CGLFrame(root, 50,50)
+CGL = CGLFrame(root, 25,25)
+root.bind('<space>',CGL.toggleUpdating)
 CGL.mainloop()
-#
-# print("Test")
-# #!/usr/bin/env python
-#
-# """Proof of concept gfxdraw example"""
-#
-# import pygame, sys
-# from pygame.locals import *
-#
-#
-# def main():
-#     pygame.init()
-#     screen = pygame.display.set_mode((400, 300))
-#     done = False
-#     red = (255, 0, 0)
-#     green = (0, 255, 0)
-#     blue = (0, 0, 255)
-#     white = (255, 255, 255)
-#     while not done:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 done = True
-#         pygame.draw.rect(screen, red, pygame.Rect(100, 30, 60, 60))
-#         pygame.draw.polygon(screen, blue, ((25, 75), (76, 125), (275, 200), (350, 25), (60, 280)))
-#         pygame.draw.circle(screen, white, (180, 180), 60)
-#         pygame.draw.line(screen, red, (10, 200), (300, 10), 4)
-#         pygame.draw.ellipse(screen, green, (250, 200, 130, 80))
-#         pygame.display.update()
-#
-#
-#
-# if __name__ == "__main__":
-#     main()
